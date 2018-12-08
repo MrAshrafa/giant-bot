1ca509f7359b3a25bc52ac192cdf7b89b82aff5d
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-08 19:18:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -328,7 +328,7 @@ client.on('message', message => {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor('RANDOM')
-  .addField("https://discordapp.com/api/oauth2/authorize?client_id=520267897659195392&permissions=8&scope=bot")
+  .addField("https://discordapp.com/api/oauth2/authorize?client_id=520267897659195392&permissions=8&scope=bot" , "هاذا رابط انفايت بوت")
   message.channel.sendEmbed(embed);
     }
 });
